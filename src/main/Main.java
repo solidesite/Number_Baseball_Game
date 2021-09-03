2747187dc93627e209d8fcce56dfc9ef40ec9375
solidesite <solidesite@gmail.com>
2021-09-03 22:34:50 +0900

    무한반복 처리

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,18 +10,9 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 		int ball = 0;
 		int strike = 0;
-		
+
 		int[] nums = new int[3];
 		boolean[] numCheck = new boolean[9];
-
-		System.out.println("0~9 사이 숫자를 입력하세요.");
-		System.out.println("첫번째 숫자를 입력하세요.");
-		String input1 = scan.nextLine();
-		System.out.println("두번째 숫자를 입력하세요.");
-		String input2 = scan.nextLine();
-		System.out.println("세번째 숫자를 입력하세요.");
-		String input3 = scan.nextLine();
-		System.out.println("입력한 숫자 : " + input1 +", "+ input2+ ", " + input3);
 
 		for (int i = 0; i < nums.length; i++) {
 			int ranNum = rd.nextInt(10);
@@ -36,27 +27,39 @@ public class Main {
 			System.out.println(nums[i]);
 		}
 
-		int inputInt1 = Integer.parseInt(input1);
-		int inputInt2 = Integer.parseInt(input2);
-		int inputInt3 = Integer.parseInt(input3);
+		while (true) {
+			System.out.println("0~9 사이 숫자를 입력하세요.");
+			System.out.println("첫번째 숫자를 입력하세요.");
+			String input1 = scan.nextLine();
+			System.out.println("두번째 숫자를 입력하세요.");
+			String input2 = scan.nextLine();
+			System.out.println("세번째 숫자를 입력하세요.");
+			String input3 = scan.nextLine();
+			System.out.println("입력한 숫자 : " + input1 + ", " + input2 + ", " + input3);
 
-		if (inputInt1 == nums[0]) {
-			strike++;
-		} else if (inputInt1 == nums[1] || inputInt1 == nums[2]) {
-			ball++;
+			int inputInt1 = Integer.parseInt(input1);
+			int inputInt2 = Integer.parseInt(input2);
+			int inputInt3 = Integer.parseInt(input3);
+
+			if (inputInt1 == nums[0]) {
+				strike++;
+			} else if (inputInt1 == nums[1] || inputInt1 == nums[2]) {
+				ball++;
+			}
+			if (inputInt2 == nums[1]) {
+				strike++;
+			} else if (inputInt2 == nums[0] || inputInt2 == nums[2]) {
+				ball++;
+			}
+			if (inputInt3 == nums[2]) {
+				strike++;
+			} else if (inputInt3 == nums[0] || inputInt3 == nums[1]) {
+				ball++;
+			}
+
+			System.out.println("볼 : " + ball + "스트라이크 : " + strike);
 		}
-		if (inputInt2 == nums[1]) {
-			strike++;
-		} else if (inputInt2 == nums[0] || inputInt2 == nums[2]) {
-			ball++;
-		}
-		if (inputInt3 == nums[2]) {
-			strike++;
-		} else if (inputInt3 == nums[0] || inputInt3 == nums[1]) {
-			ball++;
-		}
-		
-		System.out.println("볼 : " + ball + "스트라이크 : " + strike);
+
 	}
 
 }
