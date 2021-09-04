0a36773384c77129a90a6fe32c04db8d1bdc77b4
solidesite <solidesite@gmail.com>
2021-09-04 23:13:28 +0900

    횟수제한 기능 추가

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,12 +10,13 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 		int ball = 0;
 		int strike = 0;
+		int count = 8;
 
 		int[] nums = new int[3];
 		boolean[] numCheck = new boolean[9];
 
 		for (int i = 0; i < nums.length; i++) {
-			int ranNum = rd.nextInt(10);
+			int ranNum = rd.nextInt(9);
 			if (numCheck[ranNum] == false) {
 				numCheck[ranNum] = true;
 				nums[i] = ranNum + 1;
@@ -23,11 +24,16 @@ public class Main {
 				i--;
 			}
 		}
-		for (int i = 0; i < nums.length; i++) {
-			System.out.println(nums[i]);
-		}
 
 		while (true) {
+			if (count == 0) {
+				System.out.println("맞추지 못했습니다. 정답은");
+				for (int i = 0; i < nums.length; i++) {
+					System.out.println(nums[i]);
+				}
+				break;
+			}
+			System.out.println("남은 기회 : " + count + "번");
 			System.out.println("0~9 사이 숫자를 입력하세요.");
 			System.out.println("첫번째 숫자를 입력하세요.");
 			String input1 = scan.nextLine();
@@ -58,6 +64,11 @@ public class Main {
 			}
 
 			System.out.println("볼 : " + ball + "스트라이크 : " + strike);
+			if (inputInt1 == nums[0] && inputInt2 == nums[1] && inputInt3 == nums[2]) {
+				System.out.println("축하합니다. 모두 맞췄습니다.");
+				break;
+			}
+			count--;
 		}
 
 	}
